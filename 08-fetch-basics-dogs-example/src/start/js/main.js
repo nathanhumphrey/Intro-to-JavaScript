1db58ba363ba7e4a3e5a0c6078991d604319ed63
Nathan Humphrey <nathpaulhump@gmail.com>
2023-03-08 19:32:04 -0700

    adds 08-fetch-basics-dog-example

---
+++ 08-fetch-basics-dogs-example/src/start/js/main.js
@@ -0,0 +1,12 @@
+/*
+Fetch Fundamentals
+In this example, we're going to fetch and display random pictures of dogs
+1. Select the random dog button and add a click event listener on it
+2. Create a function to get a random dog image using the fetch api
+    - documentation for the dog api here https://dog.ceo/dog-api/
+    - explore and display what the promise returns
+3. Create a function that will set the image in the document to the one
+    retrieved from the dog API
+4. Call the random dog function in your click event listener,
+    and also call it when the page loads (so a dog appears right away)
+*/
